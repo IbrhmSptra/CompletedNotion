a5566d1a7949819538252071cfba135f72390129
IbrhmSptra <sptraibrhm@gmail.com>
2023-05-21 16:22:04 +0700

    first commit

---
+++ app/Views/layouts/scripts.php
@@ -0,0 +1,16 @@
+ <!-- Bootstrap core JavaScript-->
+ <script src="/vendortempt/jquery/jquery.min.js"></script>
+ <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+ <!-- Core plugin JavaScript-->
+ <script src="/vendortempt/jquery-easing/jquery.easing.min.js"></script>
+
+ <!-- Custom scripts for all pages-->
+ <script src="/assets/js/sb-admin-2.min.js"></script>
+
+ <!-- Page level plugins -->
+ <script src="/vendortempt/datatables/jquery.dataTables.min.js"></script>
+ <script src="/vendortempt/datatables/dataTables.bootstrap4.min.js"></script>
+
+ <!-- Page level custom scripts -->
+ <script src="/assets/js/demo/datatables-demo.js"></script>
